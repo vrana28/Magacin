99d663aaab343bf8e61f3159027d6ee8260cad8b
vrana28 <vranjesdjordje@gmail.com>
2021-03-14 13:00:03 +0100

    Dodat je interfejs MagacinInterfejs sa metodama dodajArtikal, izbaciArtikal i vratiInfo

---
+++ Magacin/src/artikli/interfejs/MagacinInterfejs.java
@@ -0,0 +1,11 @@
+package artikli.interfejs;
+
+import artikli.Artikal;
+
+public interface MagacinInterfejs {
+
+	public void dodajArtikal();
+	public void izbaciArtikal();
+	public Artikal vratiInfo(int sifraArtikla);
+	
+}
