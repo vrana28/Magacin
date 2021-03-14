e0ca25c0635737c56cd4c7d41f69876b3a76b5ba
vrana28 <vranjesdjordje@gmail.com>
2021-03-14 13:09:14 +0100

    Ubacena klasa za implementaciju - KlasaZaImplementaciju

---
+++ Magacin/src/artikli/KlasaZaImplementaciju.java
@@ -0,0 +1,25 @@
+package artikli;
+
+import artikli.interfejs.MagacinInterfejs;
+
+public class KlasaZaImplementaciju implements MagacinInterfejs {
+
+	@Override
+	public void dodajArtikal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal vratiInfo(int sifraArtikla) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
