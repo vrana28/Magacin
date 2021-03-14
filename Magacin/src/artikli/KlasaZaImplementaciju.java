bcaee1768c7217c1bd050da3f1a871772a6a41b8
vrana28 <vranjesdjordje@gmail.com>
2021-03-14 14:12:21 +0100

    Dodavanje stare nefunkcionalne verzije implementacije klase za imeplementaciju - KlasaZaImplementaciju

--- Magacin/src/artikli/KlasaZaImplementaciju.java
+++ Magacin/src/artikli/KlasaZaImplementaciju.java
@@ -1,25 +1,49 @@
 package artikli;
 
+import java.util.LinkedList;
+
 import artikli.interfejs.MagacinInterfejs;
 
 public class KlasaZaImplementaciju implements MagacinInterfejs {
 
 	@Override
-	public void dodajArtikal() {
-		// TODO Auto-generated method stub
+	public void dodajArtikal(Artikal a, Artikal magacin) {
+		if(a==null) throw new RuntimeException("Dodali ste artikal koji je prazan");
+		LinkedList<Artikal> lista = magacin.getListaArtikal();
+		if(lista.contains(a)) {
+			for (Artikal artikal : lista) {
+				if(a.getClass().equals(artikal.getClass())) artikal.setKolicina(artikal.getKolicina()+1);
+			}
+		}else {
+			lista.add(a);
+		}
+		magacin.setListaArtikal(lista);
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal a, Artikal magacin) {
+		if(a==null) throw new RuntimeException("Dodali ste artikal koji je prazan");
+		LinkedList<Artikal> lista = magacin.getListaArtikal();
+		if(lista.contains(a)) {
+			for (Artikal artikal : lista) {
+				if(a.getClass().equals(artikal.getClass())) artikal.setKolicina(artikal.getKolicina()-1);
+			}
+		}else {
+			throw new RuntimeException("Artikal ne postoji u magacinu, druga verzija");
+		}
+		magacin.setListaArtikal(lista);
 		
 	}
 
 	@Override
-	public void izbaciArtikal() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Artikal vratiInfo(int sifraArtikla) {
-		// TODO Auto-generated method stub
-		return null;
+	public String vratiInfo(int sifraArtikla, Artikal magacin)  {
+		LinkedList<Artikal> lista = magacin.getListaArtikal();
+		if(lista.get(sifraArtikla)!=null) {
+			Artikal a = lista.get(sifraArtikla);
+			return a.toString();
+		}else{
+			throw new RuntimeException("Greska, indeks koji ste uneli ne postoji kao artikal.");
+		}
 	}
 
 }
