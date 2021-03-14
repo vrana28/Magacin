69f978bd7d89b613da601cbd90e43eddda1007f4
vrana28 <vranjesdjordje@gmail.com>
2021-03-14 12:18:32 +0100

    Dodati atributi i get/set metode

--- Magacin/src/artikli/Knjiga.java
+++ Magacin/src/artikli/Knjiga.java
@@ -2,4 +2,35 @@ package artikli;
 
 public class Knjiga {
 
+	public String naziv;
+	public int sifra;
+	public String opis;
+	public int kolicina;
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public int getSifra() {
+		return sifra;
+	}
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	public String getOpis() {
+		return opis;
+	}
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+	public int getKolicina() {
+		return kolicina;
+	}
+	public void setKolicina(int kolicina) {
+		this.kolicina = kolicina;
+	}
+	
+	
+	
 }
--- Magacin/src/artikli/KucnaHemija.java
+++ Magacin/src/artikli/KucnaHemija.java
@@ -1,5 +1,45 @@
 package artikli;
 
+import java.sql.Date;
+
 public class KucnaHemija {
 
+	public String naziv;
+	public int sifra;
+	public String opis;
+	public int kolicina;
+	public Date rokTrajanja;
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public int getSifra() {
+		return sifra;
+	}
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	public String getOpis() {
+		return opis;
+	}
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+	public int getKolicina() {
+		return kolicina;
+	}
+	public void setKolicina(int kolicina) {
+		this.kolicina = kolicina;
+	}
+	public Date getRokTrajanja() {
+		return rokTrajanja;
+	}
+	public void setRokTrajanja(Date rokTrajanja) {
+		this.rokTrajanja = rokTrajanja;
+	}
+	
+	
+	
 }
